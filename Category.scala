87c46173d26c6ac8926d19045b2ca03040c64cd6
mirco mannucci <mirco@holomathics.com>
2023-06-20 14:32:33 -0400

    Update Category.scala
    thanks to gpt-4

--- Category.scala
+++ Category.scala
@@ -3,63 +3,48 @@ package holomathics.hyperstructures.categories
 import scalax.collection.Graph
 import scalax.collection.GraphEdge.DiEdge
 
-
-/*
-Category is a type class,.
-The idea is that it should be used to turn known types into actual categories
-* For instance, take Int: the objects are integers, and the category is given by the induced order
-* */
-
- trait Category[OB, AR]  {
-
-
-  def getObjects(): Set[OB]
-  def getArrows():  Set[AR]
-
+/**
+ * Category is a type class.
+ * It is used to convert known types into actual categories.
+ * For instance, we can use `Int` as the objects, where the category is defined by the induced order.
+ */
+trait Category[OB, AR] {
+  def getObjects: Set[OB]
+  def getArrows: Set[AR]
   def id(obj: OB): AR
+  def dom(arr: AR): OB
+  def codom(arr: AR): OB
+  
+  /**
+   * Compose two arrows. Returns `None` if the composition is not defined.
+   */
+  def compose(f: AR, g: AR): Option[AR]
+  def isEqualTo(f: AR, g: AR): Boolean
 
-
-  def dom(arr: AR) :  OB
-  def codom(arr: AR) :  OB
-  def comp( f: AR, g: AR): Option[AR]
-  def isEqual(f:AR, g:AR) : Boolean
-
-  object CateogryChecker{
-
-    def check( cat: Category[OB, AR]): Boolean ={
-
-      true
-    }
+  object CategoryChecker {
+    def check(cat: Category[OB, AR]): Boolean = true
   }
 
+  type DiEdgeType = DiEdge
+  type TupleType = Tuple[Int, Int]
+  
+  object CategoryBuilder {
+    def buildFreeCatFromGraph(graph: Graph[Int, DiEdgeType]): Category[Int, DiEdgeType] = {
+      // TODO: Implement this method
+      throw new NotImplementedError
+    }
 
-   object CategoryBuilder {
+    // Delta is the so-called simplicial category, one of the basic geometrical shape categories.
+    // object Delta extends Category[Int, TupleType]
 
+    // object Omega
 
-     def buildFreeCatFromGraph(graph: Graph[Int, DiEdge] ):
-     Category[Int, DiEdge] = ???
+    /**
+     * This method builds a quotient category from a functor.
+     */
 
-
-     /*
-   * Delta is the so-called simplicial category, one of the basic geometrical shape categories
-   *
-   * */
-
-
-     //object Delta extends Category[Int, Tuple[Int, Int]]
-
-     //object Omega
-
-     /**
-       * this method builds a quotient category from a functor
-       */
-
-
-     /***
-       *  this method builds a product category from two cats
-       */
-
-   }
-
+    /**
+     * This method builds a product category from two cats.
+     */
+  }
 }
-
